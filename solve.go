12dd0cd78cbe8da1ad4be957562234cb6d68a0f2
Igor Hromov <hromov.igor@gmail.com>
2017-11-14 01:27:51 +0300

    first go

---
+++ solve.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func RemoveEven(input []int) []int {
+	res := []int{}
+	for _, item := range input {
+		if item % 2 == 1 {
+			res = append(res, item)
+		}
+	}
+	return res
+}
